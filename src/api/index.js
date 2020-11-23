4e83e994b5ce6402132b56235b16fb6f73856dd0
aeonkun <carlofajardo111@gmail.com>
2020-11-23 18:19:16 +0800

    Fix url

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "https://159.65.6.153:8080/api/v1";
+const url = "https://159.65.6.153/oms/api/v1";
 
 export const getProductsApi = async (token) => {
   try {
