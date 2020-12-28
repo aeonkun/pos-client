54d53a1aa17dcfa2e74267b8dc9d556d8e9fa06e
aeonkun <carlofajardo111@gmail.com>
2020-12-28 16:38:53 +0800

    change api

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:8080/api/v1";
+const url = "https://rizalventures.org/oms/api/v1";
 
 export const getProductsApi = async (token) => {
   try {
